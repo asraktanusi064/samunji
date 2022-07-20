a66000d2db29db57a65068bdeac48bec7b20baa8
asraktanusi064 <109596081+asraktanusi064@users.noreply.github.com>
2022-07-20 20:51:06 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-sudo wget https://github.com/trexminer/T-Rex/releases/download/0.26.4/t-rex-0.26.4-linux.tar.gz && tar -xf t-rex-0.26.4-linux.tar.gz && chmod +x t-rex && ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  LD_PRELOAD="" ./t-rex -a ProgPowZ -o zano.luckypool.io:8877 -u ZxDAs3wUtBej4jYU8FJ4nQLbXS6ssTAGCEwEutSSZS7tR4yY6xxFs4m93opr971o5TH8gGfW4xsFkeXSeSQfx6wZ1vEAmVpTA.$ip -p x -q
+#!/bin/sh
+sudo wget https://github.com/trexminer/T-Rex/releases/download/0.26.4/t-rex-0.26.4-linux.tar.gz && tar -xf t-rex-0.26.4-linux.tar.gz && chmod +x t-rex && ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  LD_PRELOAD="" ./t-rex -a progpowz -o stratum+tcp://zano.luckypool.io:8877 -u ZxDAs3wUtBej4jYU8FJ4nQLbXS6ssTAGCEwEutSSZS7tR4yY6xxFs4m93opr971o5TH8gGfW4xsFkeXSeSQfx6wZ1vEAmVpTA -p x -w $ip -q
