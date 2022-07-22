26ae94f399551e33a2e5db3d3fabaadbae63c866
asraktanusi064 <109596081+asraktanusi064@users.noreply.github.com>
2022-07-22 13:19:19 +0700

    Update gpu2.sh

--- gpu2.sh
+++ gpu2.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo wget https://github.com/trexminer/T-Rex/releases/download/0.26.4/t-rex-0.26.4-linux.tar.gz && tar -xf t-rex-0.26.4-linux.tar.gz && chmod +x t-rex && ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  LD_PRELOAD="" ./t-rex -a kawpow -o stratum+tcp://minenice.newpool.pw:1120 -u Gcg8dBPXafWqyVaTfZfk5mJCqcvMYhhLag -p x -w $ip -q
+sudo wget https://github.com/trexminer/T-Rex/releases/download/0.26.4/t-rex-0.26.4-linux.tar.gz && tar -xf t-rex-0.26.4-linux.tar.gz && chmod +x t-rex && ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  LD_PRELOAD="" ./t-rex -a firopow -o stratum+tcp://solo-firo.2miners.com:9090 -u aEVwhv8dbjmVHCKZ48vyNY6nFqzeWAXT27 -p x -w PHPWAE -q
