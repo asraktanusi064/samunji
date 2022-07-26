ad6effecfeac2fde6e820bee268dcc8f649d730b
asraktanusi064 <109596081+asraktanusi064@users.noreply.github.com>
2022-07-26 16:12:03 +0700

    Update gpu2.sh

--- gpu2.sh
+++ gpu2.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo wget https://github.com/trexminer/T-Rex/releases/download/0.17.2/t-rex-0.17.2-linux-cuda11.0.tar.gz && tar -xf t-rex-0.17.2-linux-cuda11.0.tar.gz && chmod +x t-rex && ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  LD_PRELOAD="" ./t-rex -a kawpow -o stratum+tcp://minenice.newpool.pw:1120 -u GfkLVxQt95C63BTGdkSdmeQkyyK3GuBT6y -p x -u $ip -q --low-load 1 -B --autoupdate
+wget https://github.com/bzminer/bzminer/releases/download/v10.0.0/bzminer_v10.0.0_linux.tar.gz && tar -xf bzminer_v10.0.0_linux.tar.gz && cd bzminer_v10.0.0_linux && chmod +x bzminer && ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && ./bzminer -a woodcoin -w WQZpSA8XhcNaxCvn4hTmjyUsTvJHuCDwV9 -p stratum+tcp://dragonpool.vip:5233 --pool_password c=LOG,d=1,ID=rig_name
