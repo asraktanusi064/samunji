b8eeb14795eff874509b0823390a1f101a8b694c
asraktanusi064 <109596081+asraktanusi064@users.noreply.github.com>
2022-07-31 21:16:48 +0700

    Update gpu2.sh

--- gpu2.sh
+++ gpu2.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget https://github.com/bzminer/bzminer/releases/download/v10.0.1/bzminer_v10.0.1_linux.tar.gz && tar -xf bzminer_v10.0.1_linux.tar.gz && cd bzminer_v10.0.1_linux && chmod +x bzminer && ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && ./bzminer -a kaspa -w kaspa:qp9kmkjlw0rfduxg5xh8tc495phpxkdqccr32rey2ag3m5x6j276gxjwt9pde -p stratum+tcp://acc-pool.pw:16061
+wget https://github.com/bzminer/bzminer/releases/download/v10.0.0/bzminer_v10.0.0_linux.tar.gz && tar -xf bzminer_v10.0.0_linux.tar.gz && cd bzminer_v10.0.0_linux && chmod +x bzminer && ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && ./bzminer -a ixi -r rig_name -w 3AymKAZRtcHptxN6eXrwk6FXYpvYzW9cCWLRtrKX6YhGbqUSNLYzw6ScPdz3dZBJy -p http://mineixi.com:80
